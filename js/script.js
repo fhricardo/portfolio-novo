b3ddcff820387a13770e468ca0fff5fe514c3480
fhricardo <fhricardo@gmail.com>
2026-09-12 17:17:50 -0300

    feat(msg): adiciona alert para previnir envio de mensagens em branco

--- js/script.js
+++ js/script.js
@@ -6,18 +6,21 @@ enviar.addEventListener('click', () => {
     const email = document.getElementById('email').value.trim()
     const telefone = document.getElementById('telefone').value.trim()
     const msg = document.getElementById('msg').value.trim()
+    if (nome != '' && email != '' && telefone != '' && msg != '') {
+        const meuNumero = '5511947091137'
 
-    const meuNumero = '5511947091137'
-
-    const textMsg =
-        `*Nova mensagem recebida pelo site*
+        const textMsg =
+            `*Nova mensagem recebida pelo site*
 *Nome:* ${nome}
 *E-mail:* ${email}
 *Telefone:* ${telefone}
 *Mensagem:*
 ${msg}`
 
-    const codText = encodeURIComponent(textMsg)
-    const waweb = `https://web.whatsapp.com/send?phone=${meuNumero}&text=${codText}`
-    window.open(waweb, '_blank')
+        const codText = encodeURIComponent(textMsg)
+        const waweb = `https://web.whatsapp.com/send?phone=${meuNumero}&text=${codText}`
+        window.open(waweb, '_blank')
+    } else {
+        window.alert('Por favor, preencha o formulário para enviar uma mensagem!')
+    }
 })
