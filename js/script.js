8dcd66059c212f0802c087b5bf7289be4978cadf
fhricardo <fhricardo@gmail.com>
2026-09-26 12:41:32 -0300

    feat(responsividade): adiciona responsividade ao projeto

--- js/script.js
+++ js/script.js
@@ -86,6 +86,7 @@ function mostrarTrabalhos() {
         `).join('')
 }
 mostrarTrabalhos()
+
 // Controle do Modal
 const modalOverlay = document.getElementById('modalOverlay')
 const modalClose = document.getElementById('modalClose')
@@ -139,5 +140,29 @@ modalOverlay.addEventListener('click', (e) => {
 window.addEventListener('keydown', (e) => {
     if (e.key === 'Escape' && modalOverlay.classList.contains('active')) {
         fecharModal()
+    }
+})
+// Menu
+const width = window.innerWidth
+const btnMenu = document.getElementById('btnMenu')
+const nav = document.querySelector('nav')
+btnMenu.addEventListener('click', (e) => {
+    nav.style.display = nav.style.display === 'none' ? 'flex' : 'none'
+})
+window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        nav.style.display = nav.style.display === 'none' ? 'flex' : 'none'
+    }
+})
+document.addEventListener('click', (e) => {
+    if (!nav.contains(e.target) && !btnMenu.contains(e.target)) {
+        nav.style.display = 'none'
+    }
+})
+window.addEventListener('resize', () => {
+    if (width >= 1025) {
+        nav.style.display = 'flex'
+    } else {
+        nav.style.display = 'none'
     }
 })
